214ca0ba38fed59f596776710f9d7cd98cdaee83
Katherinemwortmano <35477712+Katherinemwortmano@users.noreply.github.com>
2018-02-01 14:00:46 -0600

    Add files via upload

---
+++ Homework 2.py
@@ -0,0 +1,29 @@
+#Maps and Lists Homework
+
+#List Questions
+
+MyList=["Assignment", "Number Two", "Intro to computer science", "Katherine", "WortmanOtto"]
+
+for Words in MyList :
+    print(Words)
+
+    
+#Map Questions 
+    
+    myMap = {0:"Assignment", 1: "Number Two", 2: "Intro to computer science", 3: "Katherine" , 4: "WortmanOtto"}
+
+    print(myMap)
+
+for loopVar in myMap:
+    
+    print(loopVar)
+    
+for loopVar in myMap: 
+    
+    print(myMap[loopVar])
+    
+    print(myMap.keys())
+    
+    print(list(myMap.keys()))
+    
+    
